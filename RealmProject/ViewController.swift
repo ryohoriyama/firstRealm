5be005fd55a7f5548f03ef65a1b2cf5012d1238a
Ryo Horiyama <>
2019-07-30 09:23:05 +0800

    firstComitt

---
+++ RealmProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmProject
+//
+//  Created by Ryo Horiyama on 2019/07/30.
+//  Copyright © 2019 Ryo Horiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
